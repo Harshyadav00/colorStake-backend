2b191d0ccf0ba557f822a73a8e5c2698755f338c
Harsh yadav <82644598+Harshyadav00@users.noreply.github.com>
2025-02-19 19:47:13 +0530

    added netlify url in security config

--- src/main/java/com/example/demo/Utility/SecurityConfig.java
+++ src/main/java/com/example/demo/Utility/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000");
+                        .allowedOrigins("http://localhost:3000", "https://colorstake.netlify.app");
 
             }
         };
